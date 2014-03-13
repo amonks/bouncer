b8a03012b09812a86459774e1f3c856c1b587928
Andrew Monks <a@monks.co>
2014-03-12 23:56:03 -0500

    lib

--- public/b.ss.cx.js
+++ public/b.ss.cx.js
@@ -1,12 +1,12 @@
 Bouncer = {
 	get: function(url) {
-		url = "http://b.ss.cx/" + url;
+		url = "http://b.youwouldntstealacar.com/" + url;
 
         var xmlHttp = null;
         xmlHttp = new XMLHttpRequest();
         xmlHttp.open("GET", url, false);
         xmlHttp.send(null);
-        
+
         return xmlHttp.responseText;
     },
 }
