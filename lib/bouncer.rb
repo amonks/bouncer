66f3746a29ebbeabf90c6537e91482a05a275b3f
Andrew Monks <a@monks.co>
2014-03-14 21:41:35 -0500

    major updates

--- lib/bouncer.rb
+++ lib/bouncer.rb
@@ -2,9 +2,9 @@
 
 module Bouncer
 
-  def errorPage(message)
-    status 404
-    @errorMessage  = message
+  def errorPage(message, code)
+    status code
+    @errorMessage = message
     haml :index
   end
 
@@ -12,24 +12,20 @@ module Bouncer
     out = fetch(url,3)
     status out.code
     response.headers["Access-Control-Allow-Origin"] = "*"
+    haml :index
     return out.body
   end
 
   def fetch(url,maxRequests)
     i = 0
-    out = String.new
-    out = httpGet(url)
+    test = String.new
+    until (test.length > 0 or i > maxRequests)
+      out = httpGet(url)
+      test = out.body
+      i += 1
+    end
+    halt errorPage(out.return_code.to_s, out.code) unless out.body.length > 0
     return out
-  end
-
-  def httpGetOld(url)
-    url = 'http://' + url unless url.include? '://'
-    checkImg(url)
-    response = HTTParty.get(url)
-    status response.code
-    return response
-  rescue
-    halt errorPage('I couldn\'t connect to that server...')
   end
 
   def httpGet(url)
@@ -41,8 +37,8 @@ module Bouncer
   end
 
   def checkImg(url)
-    halt errorPage('I don\'t proxy images...') if url.downcase.match(/\.(png|jpg|gif|tif|raw|ico)/i)
-    halt errorPage('I don\'t proxy music...') if url.downcase.match(/\.(mp|wav|aif|mov)/i)
+    halt errorPage('I don\'t proxy images...', 403) if url.downcase.match(/\.(png|jpe?g|gif|tif|raw|ico)/i)
+    halt errorPage('I don\'t proxy music...', 403) if url.downcase.match(/\.(mp|wav|aif|mov)/i)
   end
 
 end
