33ff432455cb6cc041f4b9a5a9a4a4b9d4e880c0
Andrew Monks <a@monks.co>
2014-03-19 11:17:38 -0500

    comments

--- lib/bouncer.rb
+++ lib/bouncer.rb
@@ -2,21 +2,30 @@
 
 module Bouncer
 
+  # function to show an error page
+  # accepts a message to be displayed and an http status code
   def errorPage(message, code)
     status code
     @errorMessage = message
     haml :index
   end
 
+  # main wrapper function to proxy a url
   def bounce(url)
     out = getUzi(url,3)
+    # mimic status code
     status out.code
+    # mimic content type
     content_type out.headers_hash['Content-Type']
+    # allow cross origin requests
     response.headers["Access-Control-Allow-Origin"] = "*"
+
     haml :index
     return out.body
   end
 
+  # keep requesting a url until it works
+  # accepts the url and the maximum number of tries
   def getUzi(url,maxRequests)
     i = 0
     test = String.new
@@ -29,18 +38,24 @@ module Bouncer
     return out
   end
 
+  # http get function
   def httpGet(url)
     url = 'http://' + url unless url.include? '://'
     checkImg(url)
     request = Typhoeus::Request.new(url)
+    # stream the header first to check declared size
     request.on_headers do |response|
+      # throw error if the file's too big
       halt errorPage('I don\'t proxy files larger than 10 megabytes', 403) if response.headers_hash['Content-Length'].to_i > 10485760
     end
+    # download the rest
     response = request.run
-    # binding.pry
     return response
   end
 
+
+  # check urls for unsupported file extensions
+  # I really ought to just look at content-type...
   def checkImg(url)
     halt errorPage('I don\'t proxy images...', 403) if url.downcase.match(/\.(png|jpe?g|gif|tif|raw|ico)/i)
     # halt errorPage('I don\'t proxy music...', 403) if url.downcase.match(/\.(mp|wav|aif|mov)/i)
